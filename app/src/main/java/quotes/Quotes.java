2e03d7b1bd158fa2b78eb7168bd8c1fc45e85e8c
macRos-Alzoubi <macRos.alzoubi@gmail.com>
2021-10-05 20:04:26 +0300

    fin lab-8

---
+++ app/src/main/java/quotes/Quotes.java
@@ -0,0 +1,24 @@
+package quotes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Quotes {
+    String[] tags;
+    String author;
+    String text;
+    String likes;
+
+
+    public Quotes(String[] tags, String author, String text, String likes) {
+        this.tags = tags;
+        this.author = author;
+        this.text = text;
+        this.likes = likes;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Quote{author: %s, quote: %s}\n",author,text);
+    }
+}
